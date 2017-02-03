3265205a03542959898685e7c15c98125983c146
Julius Krumbiegel <julius.krumbiegel@gmail.com>
2017-02-03 13:40:29 +0100

    changed inputs to named arguments and added clamped tuple output option

--- utils/plot.py
+++ utils/plot.py
@@ -32,9 +32,9 @@ def get_color_cycler(
         if not l_max:
             l_max = l_min
         if not a_max:
-            a_max = a_min
+            a_max = - a_min
         if not b_max:
-            b_max = b_min
+            b_max = - b_min
 
     if type == "Lch":
         rgb = get_lch_rgb(n, l_min, l_max, c_min, c_max, h_min, h_max)
@@ -49,11 +49,26 @@ def get_color_cycler(
     return cyc
 
 
-def get_lch_rgb(n_colors, l_min, l_max, c_min, c_max, h_min, h_max):
+def get_lch_rgb(
+        n=5,
+        l_min=70,
+        c_min=30,
+        h_min=0,
+        l_max=None,
+        c_max=None,
+        h_max=None,
+        clamped_tuples=False):
 
-    L = np.linspace(l_min, l_max, n_colors)
-    c = np.linspace(c_min, c_max, n_colors)
-    h = np.linspace(h_min, h_max, n_colors)
+    if not c_max:
+            c_max = c_min
+    if not h_max:
+        h_max = h_min + 360 - (360 / n)
+    if not l_max:
+        l_max = l_min
+
+    L = np.linspace(l_min, l_max, n)
+    c = np.linspace(c_min, c_max, n)
+    h = np.linspace(h_min, h_max, n)
     Lch = np.vstack((L, c, h)).T
 
     Lch_colors = [
@@ -63,22 +78,53 @@ def get_lch_rgb(n_colors, l_min, l_max, c_min, c_max, h_min, h_max):
         convert_color(Lch_color, AdobeRGBColor, target_illuminant="D50")
         for Lch_color in Lch_colors]
 
+    if clamped_tuples:
+
+        clamped_rgb_tuples = [
+            (color.clamped_rgb_r, color.clamped_rgb_g, color.clamped_rgb_b)
+            for color in rgb_colors]
+
+        return clamped_rgb_tuples
+
     return rgb_colors
 
 
-def get_lab_rgb(n_colors, l_min, l_max, a_min, a_max, b_min, b_max):
+def get_lab_rgb(
+        n=5,
+        l_min=80,
+        a_min=30,
+        b_min=30,
+        l_max=None,
+        a_max=None,
+        b_max=None,
+        clamped_tuples=False):
 
-    L = np.linspace(l_min, l_max, n_colors)
-    a = np.linspace(a_min, a_max, n_colors)
-    b = np.linspace(b_min, b_max, n_colors)
+    if not l_max:
+        l_max = l_min
+    if not a_max:
+        a_max = - a_min
+    if not b_max:
+        b_max = - b_min
+
+    L = np.linspace(l_min, l_max, n)
+    a = np.linspace(a_min, a_max, n)
+    b = np.linspace(b_min, b_max, n)
     Lab = np.vstack((L, a, b)).T
 
     Lab_colors = [
         LabColor(Lab[i, 0], Lab[i, 1], Lab[i, 2])
         for i in range(Lab.shape[0])]
     rgb_colors = [
-        convert_color(Lab_color, AdobeRGBColor)
+        convert_color(Lab_color, AdobeRGBColor, target_illuminant="D50")
         for Lab_color in Lab_colors]
+
+    if clamped_tuples:
+
+        clamped_rgb_tuples = [
+            (color.clamped_rgb_r, color.clamped_rgb_g, color.clamped_rgb_b)
+            for color in rgb_colors]
+
+        return clamped_rgb_tuples
 
     return rgb_colors
 
